9062ebf05547284b587953e6d3f0af5ee3f21fe0
SupperSlonic <supperslonic@gmail.com>
2014-11-13 22:51:25 -0600

    db project

---
+++ AuthDB/InitDB.sql
@@ -0,0 +1,12 @@
+﻿set identity_insert [dbo].[ExternalLoginProvider] on
+insert into [dbo].[ExternalLoginProvider]
+(
+	Id,
+	Name
+)
+values
+(1, 'Google'),
+(2, 'Facebook'),
+(3, 'Microsoft')
+
+set identity_insert [dbo].[ExternalLoginProvider] off
